20bdbd4c766899afa8b783bccb756554a8e9755b
dimas <dimasnugrohopro22@gmail.com>
2025-10-07 19:34:38 +0700

    modified gridview

--- README.md
+++ README.md
@@ -14,3 +14,12 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+Dokumentasi Hasil Running Praktikum
+Hasil run pertama aplikasi setelah dibuat(Praktikum 5)
+1. ![Tampilan Homepage](images/belanja01.png)
+2. ![Tampilan Item - Sugar](images/belanja02.png)
+3. ![Tampilan item - Salt](images/belanja03.png)
+
+Hasil run aplikasi setelah dibuat(Tugas Praktikum 2)
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,25 +13,42 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(title: Text('Home Page')),
       body: Container(
         margin: EdgeInsets.all(8),
-        child: ListView.builder(
-          padding: EdgeInsets.all(8),
+        padding: EdgeInsets.all(8),
+        child: GridView.builder(
           itemCount: items.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 2 / 1,
+            mainAxisSpacing: 8,
+            crossAxisSpacing: 8,
+          ),
           itemBuilder: (context, index) {
             return InkWell(
               onTap: () {
                 Navigator.pushNamed(context, '/item', arguments: items[index]);
               },
               child: Card(
+                // mengatur card
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
                 child: Container(
                   margin: const EdgeInsets.all(8),
-                  child: Row(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    //ubah row menjadi column
                     children: [
-                      Expanded(child: Text(items[index].name)),
-                      Expanded(
-                        child: Text(
-                          'Rp ${items[index].price}',
-                          textAlign: TextAlign.end,
+                      Text(
+                        items[index].name,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
                         ),
+                      ),
+                      Text(
+                        'Price: ${items[index].price}',
+                        style: TextStyle(fontSize: 14),
                       ),
                     ],
                   ),
